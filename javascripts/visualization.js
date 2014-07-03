1d0a094bf25c6dd1acda273bcd1dae3bb35bf59e
pantsonsteven <pantsonsteven@gmail.com>
2014-07-03 12:49:57 -0400

    removed all node stuff, reorganized file structure, created csv file of movie data, created colorbrewer.js fils

---
+++ javascripts/visualization.js
@@ -0,0 +1,248 @@
+boxOffice =    [
+   [
+      {"date": "June 1, 2014"},
+      {"movies": 
+         [
+            {"movie":"X-Men: Days of Future Past","grossSales":9339859 },
+            {"movie":"Maleficent","grossSales":19383198 },
+            {"movie":"A Million Ways to Die in The West","grossSales":4231615 },
+            {"movie":"Godzilla","grossSales":3417587 },
+            {"movie":"Neighbors","grossSales":2129605 },
+            {"movie":"Blended","grossSales":1996403 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":1091519 },
+            {"movie":"Million Dollar Arm","grossSales":950909 },
+            {"movie":"Chef","grossSales":524109 },
+            {"movie":"Rio 2","grossSales":354959 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 2, 2014"},
+      {"movies":
+         [
+            {"movie":"Maleficent","grossSales":6207742 },
+            {"movie":"X-Men: Days of Future Past","grossSales":3163908 },
+            {"movie":"A Million Ways to Die in The West","grossSales":1627855 },
+            {"movie":"Godzilla","grossSales":1152979 },
+            {"movie":"Neighbors","grossSales":911525 },
+            {"movie":"Blended","grossSales":709031 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":378342 },
+            {"movie":"Million Dollar Arm","grossSales":344767 },
+            {"movie":"Chef","grossSales":206211 },
+            {"movie":"The Other Woman","grossSales":158352 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 3, 2014"},
+      {"movies":
+         [
+            {"movie":"Maleficent","grossSales":7196208 },
+            {"movie":"X-Men: Days of Future Past","grossSales":3787895 },
+            {"movie":"A Million Ways to Die in The West","grossSales":1864635 },
+            {"movie":"Godzilla","grossSales":1370135 },
+            {"movie":"Neighbors","grossSales":1059450 },
+            {"movie":"Blended","grossSales":918371 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":501119 },
+            {"movie":"Million Dollar Arm","grossSales":429362 },
+            {"movie":"Chef","grossSales":237294 },
+            {"movie":"The Other Woman","grossSales":190344 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 4, 2014"},
+      {"movies":
+         [
+            {"movie":"Maleficent","grossSales":5612603 },
+            {"movie":"X-Men: Days of Future Past","grossSales":2824137 },
+            {"movie":"A Million Ways to Die in The West","grossSales":1419590 },
+            {"movie":"Godzilla","grossSales":1117001 },
+            {"movie":"Neighbors","grossSales":897550 },
+            {"movie":"Blended","grossSales":762465 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":402062 },
+            {"movie":"Million Dollar Arm","grossSales":399096 },
+            {"movie":"Chef","grossSales":244412 },
+            {"movie":"The Other Woman","grossSales":170963 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 5, 2014"},
+      {"movies":
+         [
+            {"movie":"Maleficent","grossSales":5399117 },
+            {"movie":"X-Men: Days of Future Past","grossSales":2605125 },
+            {"movie":"A Million Ways to Die in The West","grossSales":1280505 },
+            {"movie":"Godzilla","grossSales":1013205 },
+            {"movie":"Neighbors","grossSales":765605 },
+            {"movie":"Blended","grossSales":718238 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":370971 },
+            {"movie":"Million Dollar Arm","grossSales":366752 },
+            {"movie":"Chef","grossSales":247348 },
+            {"movie":"Belle","grossSales":158169 }
+         ]
+      }      
+   ],
+   [
+      {"date": "June 6, 2014"},
+      {"movies": 
+         [
+            {"movie":"The Fault in Our Stars","grossSales":26062046 },
+            {"movie":"Edge of Tomorrow","grossSales":10605165 },
+            {"movie":"Maleficent","grossSales":10189870 },
+            {"movie":"X-Men: Days of Future Past","grossSales":4510017 },
+            {"movie":"A Million Ways to Die in The West","grossSales":2260790 },
+            {"movie":"Godzilla","grossSales":1781428 },
+            {"movie":"Neighbors","grossSales":1645835 },
+            {"movie":"Blended","grossSales":1282320 },
+            {"movie":"Chef","grossSales":720232 },
+            {"movie":"Million Dollar Arm","grossSales":616395 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 7, 2014"},
+      {"movies": 
+         [
+            {"movie":"Maleficent","grossSales":13463278 },
+            {"movie":"The Fault in Our Stars","grossSales":12680655 },
+            {"movie":"Edge of Tomorrow","grossSales":10474234 },
+            {"movie":"X-Men: Days of Future Past","grossSales":6176540 },
+            {"movie":"A Million Ways to Die in The West","grossSales":2908685 },
+            {"movie":"Godzilla","grossSales":2509131 },
+            {"movie":"Neighbors","grossSales":2180550 },
+            {"movie":"Blended","grossSales":1699365 },
+            {"movie":"Chef","grossSales":1105294 },
+            {"movie":"Million Dollar Arm","grossSales":853302 }
+         ]
+      }   
+   ],
+   [
+      {"date": "June 8, 2014"},
+      {"movies":
+         [
+            {"movie":"Maleficent","grossSales":10675116 },
+            {"movie":"The Fault in Our Stars","grossSales":9259822 },
+            {"movie":"Edge of Tomorrow","grossSales":7680847 },
+            {"movie":"X-Men: Days of Future Past","grossSales":4468257 },
+            {"movie":"A Million Ways to Die in The West","grossSales":2179020 },
+            {"movie":"Godzilla","grossSales":1830620 },
+            {"movie":"Neighbors","grossSales":1498770 },
+            {"movie":"Blended","grossSales":1140718 },
+            {"movie":"Chef","grossSales":796303 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":598078 }
+         ]
+      }
+   ],
+   [
+      {"date": "June 9, 2014"},
+      {"movies":
+         [
+            {"movie":"The Fault in Our Stars","grossSales":5095488 },
+            {"movie":"Maleficent","grossSales":4047865 },
+            {"movie":"Edge of Tomorrow","grossSales":3167460 },
+            {"movie":"X-Men: Days of Future Past","grossSales":1765307 },
+            {"movie":"A Million Ways to Die in The West","grossSales":917935 },
+            {"movie":"Godzilla","grossSales":752166 },
+            {"movie":"Neighbors","grossSales":736680 },
+            {"movie":"Blended","grossSales":505085 },
+            {"movie":"Chef","grossSales":326484 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":271058 }
+         ]
+      }      
+   ],
+   [
+      {"date": "June 10, 2014"},
+      {"movies":
+         [
+            {"movie":"The Fault in Our Stars","grossSales":5002851 },
+            {"movie":"Maleficent","grossSales":4712585 },
+            {"movie":"Edge of Tomorrow","grossSales":3482263 },
+            {"movie":"X-Men: Days of Future Past","grossSales":2021629 },
+            {"movie":"A Million Ways to Die in The West","grossSales":1058440 },
+            {"movie":"Godzilla","grossSales":855132 },
+            {"movie":"Neighbors","grossSales":800870 },
+            {"movie":"Blended","grossSales":611112 },
+            {"movie":"Chef","grossSales":403101 },
+            {"movie":"The Amazing Spider-Man 2","grossSales":315898 }
+         ]
+      }
+   ]
+];
+
+function buildSVG(data) {
+   var margin  = {
+      top      : 100,
+      right    : 100,
+      bottom   : 100,
+      left     : 150
+      },
+      width    = 1300 - margin.right - margin.left,
+      height   = 750 - margin.top - margin.bottom,
+
+      maxSales = d3.max(boxOffice, function(day){
+         return d3.max(day[1].movies, function(movie) {
+            return movie.grossSales;
+         });
+      }),
+
+      minSales = d3.min(boxOffice, function(day){
+         return d3.min(day[1].movies, function(movie) {
+            return movie.grossSales;
+         });
+      });   
+
+   window.yAxisScale = d3.scale.linear()
+      .domain([maxSales + (.1*maxSales), 0])
+      .range([0, height]);
+
+   window.xAxisScale = d3.scale.linear()
+      .domain([0, boxOffice.length])
+      .range([0, width]);
+
+   window.svg = d3.select('body')
+      .append('svg')
+         .attr('width', width + margin.right + margin.left)
+         .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+         .attr('width', width)
+         .attr('height', height)
+         .attr('class', 'graph')
+         .attr('transform', 'translate('+margin.left+','+margin.top+')');
+
+   var xAxis = d3.svg.axis()
+      .scale(window.xAxisScale)
+      .orient('bottom');
+
+   var yAxis = d3.svg.axis()
+      .scale(window.yAxisScale)
+      .orient('right');
+
+   window.svg.append('g')
+         .attr('class', 'x axis')
+         .attr('transform', 'translate(0,'+height+')')
+         .call(xAxis)
+         .append('text')
+      .text('Date')
+         .attr('x', width/2)
+         .attr('y', 50);
+
+   window.svg.append('g')
+         .attr('class', 'y axis')
+         .attr('transform', 'translate('+width+', 0)')
+         .call(yAxis)
+         .append('text')
+      .text('Movie')
+         .attr('x', 100)
+         .attr('y', height/2);
+
+}; // end of buildSVG
+
+
+
+$(function() {
+   buildSVG(boxOffice);
+});
+
+
